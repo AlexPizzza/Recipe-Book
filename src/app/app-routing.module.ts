fef5ba794f5ede1c111af9326b9e0a3c4deda2e1
Alex Pita <alexandru.pita@bearingpoint.com>
2022-01-11 10:31:37 +0200

    add auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { RecipeStartComponentComponent } from "./recipes/recipe-start-component/
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
 import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
 import { RecipeResolverService } from "./recipes/recipe-resolver.service";
+import { AuthComponent } from "./auth/auth.component";
 
 const appRoutes: Routes = [
     {
@@ -41,6 +42,10 @@ const appRoutes: Routes = [
     {
         path: 'shopping-list',
         component: ShoppingListComponent
+    },
+    {
+        path: 'auth',
+        component: AuthComponent
     }
 ];
 
